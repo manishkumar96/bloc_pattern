db208b9ad8d8e9db24091370d9def5036dd408a9
Manish Kumar <manish96.apptunix@gmail.com>
2021-10-30 12:49:21 +0530

    30 Oct

--- lib/helpers/api_constants.dart
+++ lib/helpers/api_constants.dart
@@ -1,7 +1,6 @@
 class ApiConstants{
-static const String BASE_URL = "https://dino-safari.herokuapp.com/api/";
 
-static const String baseUrl1 = "https://partya-app.herokuapp.com/api/";
+static const String baseUrl1 = "your base url";
 
 static const String API_POST_SIGN_UP = BASE_URL + "users";
 
